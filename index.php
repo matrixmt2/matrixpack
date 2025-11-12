4c60ee52dc5ee69f0d53045306dbb5467aaaf593
SuatAyhan <suatayhan48@gmail.com>
2025-11-12 16:00:04 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html dir="ltr">
+	<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="expires" content="0">
+        <title>Metin2</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="assets/css/style.css">
+        <!--[if IE 7]>
+        <link rel="stylesheet" href="assets/css/ie7.css">
+        <![endif]-->
+    </head>
+    <body style="background: url(assets/images/background.jpg) no-repeat;">
+        <div id="content_wrapper">
+            <div id="logo_wrapper">
+                <img src="assets/images/logo.png" alt="Server Adı">
+            </div>
+            <div id="navi_wrapper1">
+                <ul id="navi1" class="navi">
+                    <li class="first_li"><a href="http://www.casro2.com/kullanici/hesap" target="_blank">HESABIM</a></li>
+                    <li><a href="#" target="_blank">ETKİNLİKLER</a></li>
+                </ul>
+            </div>
+            <div class="clearfix">
+                <div id="navi_wrapper2">
+                    <ul id="navi2" class="navi">
+                        <li class="first_li"><a href="http://www.casro2.com/wiki" target="_blank">WİKİ</a></li>
+                        <li><a href="http://www.casro2.com/kullanici/hesap/destek-sistemi" target="_blank">DESTEK</a></li>
+                        <li><a href="http://www.casro2.com/siralamalar/karakterler" target="_blank">SIRALAMA LİSTESİ</a></li>
+                        <!--li><a href="#" target="_blank">KÜNYE</a></li>
+                        <li><a href="#" target="_blank">GİŞ</a></li>
+                        <li><a href="#" target="_blank">GİZLİLİK POLİTİKASI BEYANI</a></li-->
+                        <li><a href="https://www.facebook.com/casro2" target="_blank"><img src="assets/images/facebook.png" alt="FACEBOOK" width="20" height="20"></a></li>
+                    </ul>
+                </div>
+            </div>
+			<div id="double-single" class="panel_wrapper">
+				<ul class="clearfix">
+					<li class="panel">
+						<img src="assets/images/haber/yaban_domuzu.jpg" width="694" height="309">
+						<div class="caption">
+							<h3>Savaş yaban domuzu - benzersiz ve çok tehlikeli!</h3>
+							<p>Sadece kısa süreliğine nesne markette: Popüler savaş yaban domuzu şansı içeren esrarengiz sandık. Hemen al!</p>
+						</div>
+					</li>
+					<li class="panel">
+						<img src="assets/images/haber/ice_cream.jpg" width="341" height="309">
+						<div class="caption">
+							<h3>Ice Ice Baby</h3>
+							<p>Şimdi marketten havalı silah görünümü ile yaz etkinliği için kristal kaseyi kapın!</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+        </div>
+	</body>
+</html>
---
+++ metin2_accept/index.php
@@ -0,0 +1,67 @@
+﻿
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body {
+				background: #000;
+				color: #fff;
+				font: 13px/21px normal Arial, Helvetica;
+				margin: 0;
+				padding: 0;
+			}
+			h2 {
+				color: #59b1c1;
+				font-family: 'Quattrocento Roman', serif;
+				font-size: 24px;
+			}
+			a {
+				color: #59b1c1;
+				text-decoration: underline;
+			}
+			a:hover {text-decoration: none;}
+			.gameguard-main {
+				background: url(img/gameguard_bg.jpg) 0 0 no-repeat;
+				padding: 180px 16px 0;
+				width: 638px;
+			}
+			.content {
+				background: url(img/black0_8.png);
+				padding: 10px 15px;
+			}
+			.protect {
+				float: left;
+				margin: 0 20px 20px 0;
+			}
+			.clearfix:after {
+			    content: ".";
+			    clear: both;
+			    display: block;
+			    visibility: hidden;
+			    height: 0px;
+			}
+            .rtl .gameguard-main {
+                direction: rtl;
+            }
+            .rtl .protect {
+				float: right;
+				margin: 0 0 20px 20px;
+            }
+		</style>
+	</head>
+	<body>
+		<div class="gameguard-main">
+			<div class="content"><h2>Hack'lere karşı tek yürek</h2>
+<p>Sevgili oyuncular,<br>
+Sana mümkün olan en iyi oyun deneyimini yaşatabilmek için Metin2'yi, botlar ve hack araçları gibi izinsiz ek programlara karşı bir koruma yazılımıyla güvenceye aldık. Bunun için GameGuard yazılımını kullanacağız.</p>
+<p>Gameguard programı, otomatik olarak arka planda kurulacak. Tarafından herhangi bir yardımcı program indirip kurmana gerek kalmayacak. Bu koruyucu yazılımın etkinliğini garanti altına almak için, tüm oyuncular kullanımı kabul etmesi gerekir. Bu uygulamayı anlayışla karşıladığın için şimdiden teşekkür ederiz.</p>
+<p>GameGuard'ın çalışma prensibi ve görevleri <a href="http://agbserver.gameforge.com/trTR-Privacy-Metin2.html">Veri Gizliliği'nde</a> belirtilmiştir.</p>
+<div>
+<img class="protect clearfix" src="img/NProtect_GameGuard_logo.png">
+<p>Daha fazla bilgiyi <a href="http://board.tr.metin2.gameforge.com/">forumda</a> bulabilirsin.</p>
+<p>Metin2'yi oynamak için GameGuard'ın kurulumunu kabul etmen gerekiyor. Devam etmek için <strong>"kabul et"</strong> butonuna tıkla.</p>
+</div></div>
+		</div>
+	</body>
+</html>
